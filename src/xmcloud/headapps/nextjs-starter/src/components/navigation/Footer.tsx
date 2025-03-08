db618bd6e9ea749d8d9598047f3209228722ac88
Renan Garcia <renan.garcia@verndale.com>
2025-03-08 16:35:12 -0500

    markup changes

--- src/xmcloud/headapps/nextjs-starter/src/components/navigation/Footer.tsx
+++ src/xmcloud/headapps/nextjs-starter/src/components/navigation/Footer.tsx
@@ -10,15 +10,15 @@ export default function Footer(): JSX.Element {
       <div className={styles['footer__container']}>
         <div className={styles['footer__grid']}>
           <div className={styles['footer__section']}>
-            <Link href="/" className={styles['footer__logo_link']}>
+            <Link href="/" className={styles['footer__logo-link']}>
               <img
                 src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/xmboys-I9Z2WL6vvb6Eir7aq8KRZJG7lpLijW.webp"
                 alt="XM BOYS Logo"
                 width={80}
                 height={80}
-                className={styles['footer__logo_image']}
+                className={styles['footer__logo-image']}
               />
-              <span className={styles['footer__logo_text']}>XM BOYS</span>
+              <span className={styles['footer__logo-text']}>XM BOYS</span>
             </Link>
             <p className={styles['footer__description']}>
               Delivering innovative digital solutions and exceptional experiences for our clients.
@@ -27,16 +27,16 @@ export default function Footer(): JSX.Element {
 
           <div className={styles['footer__section']}>
             <h3 className={styles['footer__heading']}>Contact Us</h3>
-            <div className={styles['footer__contact_list']}>
-              <div className={styles['footer__contact_item']}>
+            <div className={styles['footer__contact-list']}>
+              <div className={styles['footer__contact-item']}>
                 <Mail size={18} className={styles['footer__icon']} />
                 <span>info@xmboys.com</span>
               </div>
-              <div className={styles['footer__contact_item']}>
+              <div className={styles['footer__contact-item']}>
                 <Phone size={18} className={styles['footer__icon']} />
                 <span>+1 (555) 123-4567</span>
               </div>
-              <div className={styles['footer__contact_item']}>
+              <div className={styles['footer__contact-item']}>
                 <MapPin size={18} className={styles['footer__icon']} />
                 <span>123 Tech Street, Digital City</span>
               </div>
@@ -45,20 +45,20 @@ export default function Footer(): JSX.Element {
 
           <div className={styles['footer__section']}>
             <h3 className={styles['footer__heading']}>Connect With Us</h3>
-            <div className={styles['footer__social_list']}>
-              <Link href="#" className={styles['footer__social_link']}>
+            <div className={styles['footer__social-list']}>
+              <Link href="#" className={styles['footer__social-link']}>
                 <Facebook size={24} />
                 <span className="sr-only">Facebook</span>
               </Link>
-              <Link href="#" className={styles['footer__social_link']}>
+              <Link href="#" className={styles['footer__social-link']}>
                 <Twitter size={24} />
                 <span className="sr-only">Twitter</span>
               </Link>
-              <Link href="#" className={styles['footer__social_link']}>
+              <Link href="#" className={styles['footer__social-link']}>
                 <Instagram size={24} />
                 <span className="sr-only">Instagram</span>
               </Link>
-              <Link href="#" className={styles['footer__social_link']}>
+              <Link href="#" className={styles['footer__social-link']}>
                 <Youtube size={24} />
                 <span className="sr-only">YouTube</span>
               </Link>
--- src/xmcloud/headapps/nextjs-starter/src/components/navigation/Header.tsx
+++ src/xmcloud/headapps/nextjs-starter/src/components/navigation/Header.tsx
@@ -8,39 +8,41 @@ export default function Header() {
       {/* Decorative elements that match the logo's geometric style */}
       <div className={styles['header__decorative_elements']}>
         <div
-          className={`${styles['header__decorative_circle']} ${styles['header__decorative-circle--1']} ${styles['header__decorative-circle--blue']}`}
+          className={`${styles['header__decorative-circle']} ${styles['header__decorative-circle--1']} ${styles['header__decorative-circle--blue']}`}
         ></div>
         <div
-          className={`${styles['header__decorative_circle']} ${styles['header__decorative-circle--2']} ${styles['header__decorative-circle--yellow']}`}
+          className={`${styles['header__decorative-circle']} ${styles['header__decorative-circle--2']} ${styles['header__decorative-circle--yellow']}`}
         ></div>
         <div
-          className={`${styles['header__decorative_circle']} ${styles['header__decorative-circle--3']} ${styles['header__decorative-circle--blue']}`}
+          className={`${styles['header__decorative-circle']} ${styles['header__decorative-circle--3']} ${styles['header__decorative-circle--blue']}`}
         ></div>
         <div
-          className={`${styles['header__decorative_circle']} ${styles['header__decorative-circle--4']} ${styles['header__decorative-circle--yellow']}`}
+          className={`${styles['header__decorative-circle']} ${styles['header__decorative-circle--4']} ${styles['header__decorative-circle--yellow']}`}
         ></div>
       </div>
 
       {/* Subtle gradient overlay */}
-      <div className={styles['header__gradient_overlay']}></div>
+      <div className={styles['header__gradient-overlay']}></div>
 
       <div className={styles['header__container']}>
         <div className={styles['header__content']}>
           {/* Logo with enhanced presentation */}
-          <Link href="/" className={styles['header__logo_link']}>
-            <div className={styles['header__logo_wrapper']}>
-              <div className={styles['header__logo_glow']}></div>
+          <Link href="/" className={styles['header__logo-link']}>
+            <div className={styles['header__logo-wrapper']}>
+              <div className={styles['header__logo-glow']}></div>
               <img
                 src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/xmboys-I9Z2WL6vvb6Eir7aq8KRZJG7lpLijW.webp"
                 alt="XM BOYS Logo"
                 width={60}
                 height={60}
-                className={styles['header__logo_image']}
+                className={styles['header__logo-image']}
               />
             </div>
-            <div className={styles['header__logo_content']}>
-              <span className={styles['header__logo_title']}>XM BOYS</span>
-              <span className={styles['header__logo_subtitle']}>Digital Innovation</span>
+            <div className={styles['header__logo-content']}>
+              <span className={styles['header__logo-title']}>XM BOYS</span>
+              <span className={styles['header__logo-subtitle']}>
+                Delivering innovative digital solutions and exceptional experiences for our clients
+              </span>
             </div>
           </Link>
 
@@ -49,16 +51,16 @@ export default function Header() {
             href="https://github.com/Sitecore-Hackathon/2025-XM-Boys"
             target="_blank"
             rel="noopener noreferrer"
-            className={styles['header__github_link']}
+            className={styles['header__github-link']}
           >
-            <Github size={18} className={styles['header__github_icon']} />
-            <span className={styles['header__github_text']}>Visit Our Projects</span>
+            <Github size={18} className={styles['header__github-icon']} />
+            <span className={styles['header__github-text']}>Visit Our Projects</span>
           </a>
         </div>
       </div>
 
       {/* Subtle bottom border with gradient */}
-      <div className={styles['header__bottom_border']}></div>
+      <div className={styles['header__bottom-border']}></div>
     </header>
   );
 }

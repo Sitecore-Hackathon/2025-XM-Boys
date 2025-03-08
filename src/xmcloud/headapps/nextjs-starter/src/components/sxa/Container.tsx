35d5c1ae758e2efe1958081fbdffc56bd2a9af45
Roberto Armas <roberto.armas@verndale.com>
2025-03-08 16:17:18 -0500

    bugfix

--- src/xmcloud/headapps/nextjs-starter/src/components/sxa/Container.tsx
+++ src/xmcloud/headapps/nextjs-starter/src/components/sxa/Container.tsx
@@ -11,35 +11,33 @@ interface ComponentProps {
   params: ComponentParams;
 }
 
-const DefaultContainer = withEditableToolsWrapper()<ComponentProps>(
-  (props: ComponentProps): JSX.Element => {
-    const containerStyles = props?.params?.Styles ?? '';
-    const styles = `${props?.params?.GridParameters} ${containerStyles}`.trimEnd();
-    const phKey = `container-${props?.params?.DynamicPlaceholderId}`;
-    const id = props?.params?.RenderingIdentifier;
-    const mediaUrlPattern = new RegExp(/mediaurl=\"([^"]*)\"/, 'i');
-    const backgroundImage = props?.params?.BackgroundImage as string;
-    let backgroundStyle: { [key: string]: string } = {};
+const DefaultContainer = (props: ComponentProps): JSX.Element => {
+  const containerStyles = props?.params?.Styles ?? '';
+  const styles = `${props?.params?.GridParameters} ${containerStyles}`.trimEnd();
+  const phKey = `container-${props?.params?.DynamicPlaceholderId}`;
+  const id = props?.params?.RenderingIdentifier;
+  const mediaUrlPattern = new RegExp(/mediaurl=\"([^"]*)\"/, 'i');
+  const backgroundImage = props?.params?.BackgroundImage as string;
+  let backgroundStyle: { [key: string]: string } = {};
 
-    if (backgroundImage && backgroundImage.match(mediaUrlPattern)) {
-      const mediaUrl = backgroundImage.match(mediaUrlPattern)?.[1] || '';
+  if (backgroundImage && backgroundImage.match(mediaUrlPattern)) {
+    const mediaUrl = backgroundImage.match(mediaUrlPattern)?.[1] || '';
 
-      backgroundStyle = {
-        backgroundImage: `url('${mediaUrl}')`,
-      };
-    }
+    backgroundStyle = {
+      backgroundImage: `url('${mediaUrl}')`,
+    };
+  }
 
-    return (
-      <div className={`component container-default ${styles}`} id={id ? id : undefined}>
-        <div className="component-content" style={backgroundStyle}>
-          <div className="row">
-            <Placeholder name={phKey} rendering={props.rendering} />
-          </div>
+  return (
+    <div className={`component container-default ${styles}`} id={id ? id : undefined}>
+      <div className="component-content" style={backgroundStyle}>
+        <div className="row">
+          <Placeholder name={phKey} rendering={props.rendering} />
         </div>
       </div>
-    );
-  }
-);
+    </div>
+  );
+};
 
 export const Default = withEditableToolsWrapper()<ComponentProps>(
   (props: ComponentProps): JSX.Element => {
